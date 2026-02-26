7649582e1d647fb19fde005c7ca1f7f70d9ad4a0
Oumar92140 <oumar.barry09@icloud.com>
2026-02-26 08:10:39 +0100

    Delete Dockerproject/db directory

--- Dockerproject/db/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF.NOT EXISTS messages (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    author VARCHAR(100) NOT NULL,
-    content TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-INSERT INTO messages (author, content)
-VALUES ('Admin', 'Bienvenue sur le livre d\'or !');
